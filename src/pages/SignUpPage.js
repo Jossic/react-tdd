e1934e7e4c9d334fffffab5ba15982848f5310d8
Jossic <jossic.lapierre@gmail.com>
2022-01-21 22:04:56 +0100

    Testing sending body to back

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import e from 'express';
 
 const SignUpPage = () => {
 	const [values, setValues] = useState({});
@@ -8,36 +10,63 @@ const SignUpPage = () => {
 	if (password && passwordRepeat) {
 		disabled = password !== passwordRepeat;
 	}
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		const { username, email, password } = values;
+		const body = { username, email, password };
+		axios.post('/api/1.0/users', body);
+	};
 	return (
 		<>
 			<h1>Sign Up</h1>
-			<label htmlFor='username'>Username</label>
-			<input id='username' />
-			<label htmlFor='email'>E-mail</label>
-			<input id='email' />
-			<label htmlFor='password'>Password</label>
-			<input
-				id='password'
-				type='password'
-				onChange={(e) =>
-					setValues({
-						...values,
-						password: e.target.value,
-					})
-				}
-			/>
-			<label htmlFor='passwordRepeat'>Password Repeat</label>
-			<input
-				id='passwordRepeat'
-				type='password'
-				onChange={(e) =>
-					setValues({
-						...values,
-						passwordRepeat: e.target.value,
-					})
-				}
-			/>
-			<button disabled={disabled}>Sign Up</button>
+			<form>
+				<label htmlFor='username'>Username</label>
+				<input
+					id='username'
+					onChange={(e) =>
+						setValues({
+							...values,
+							username: e.target.value,
+						})
+					}
+				/>
+				<label htmlFor='email'>E-mail</label>
+				<input
+					id='email'
+					onChange={(e) =>
+						setValues({
+							...values,
+							email: e.target.value,
+						})
+					}
+				/>
+				<label htmlFor='password'>Password</label>
+				<input
+					id='password'
+					type='password'
+					onChange={(e) =>
+						setValues({
+							...values,
+							password: e.target.value,
+						})
+					}
+				/>
+				<label htmlFor='passwordRepeat'>Password Repeat</label>
+				<input
+					id='passwordRepeat'
+					type='password'
+					onChange={(e) =>
+						setValues({
+							...values,
+							passwordRepeat: e.target.value,
+						})
+					}
+				/>
+				<button disabled={disabled} onClick={(e) => onSubmit(e)}>
+					Sign Up
+				</button>
+			</form>
 		</>
 	);
 };
--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -1,6 +1,7 @@
 import SignUpPage from './SignUpPage';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import axios from 'axios';
 
 describe('>SignUp Page', () => {
 	describe('>Layout', () => {
@@ -63,10 +64,39 @@ describe('>SignUp Page', () => {
 			const passwordInput = screen.getByLabelText('Password');
 			const passwordRepeatInput =
 				screen.getByLabelText('Password Repeat');
-			userEvent.type(passwordInput, 'P4ssword');
-			userEvent.type(passwordRepeatInput, 'P4ssword');
+			userEvent.type(passwordInput, 'Password');
+			userEvent.type(passwordRepeatInput, 'Password');
 			const button = screen.queryByRole('button', { name: 'Sign Up' });
 			expect(button).toBeEnabled();
+		});
+		it('should send username, email, password to back after clicking button', () => {
+			render(<SignUpPage />);
+			const usernameInput = screen.getByLabelText('Username');
+			const emailInput = screen.getByLabelText('E-mail');
+			const passwordInput = screen.getByLabelText('Password');
+			const passwordRepeatInput =
+				screen.getByLabelText('Password Repeat');
+			userEvent.type(usernameInput, 'user1');
+			userEvent.type(emailInput, 'user1@mail.com');
+			userEvent.type(passwordInput, 'Password');
+			userEvent.type(passwordRepeatInput, 'Password');
+			const button = screen.queryByRole('button', { name: 'Sign Up' });
+
+			const mockFn = jest.fn();
+			// console.log(`mockFn =>`, mockFn)
+			axios.post = mockFn;
+
+			userEvent.click(button);
+
+			const firstCallOfMockFunction = mockFn.mock.calls[0];
+
+			const body = firstCallOfMockFunction[1];
+			// console.log(`body =>`, body);
+			expect(body).toEqual({
+				username: 'user1',
+				email: 'user1@mail.com',
+				password: 'Password',
+			});
 		});
 	});
 });
