7e0b90c9d60aa88251a8a54759082785a05d4628
Jossic <jossic.lapierre@gmail.com>
2022-01-22 13:49:41 +0100

    add bootstrap

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -18,56 +18,85 @@ const SignUpPage = () => {
 		axios.post('/api/1.0/users', body);
 	};
 	return (
-		<>
-			<h1>Sign Up</h1>
-			<form>
-				<label htmlFor='username'>Username</label>
-				<input
-					id='username'
-					onChange={(e) =>
-						setValues({
-							...values,
-							username: e.target.value,
-						})
-					}
-				/>
-				<label htmlFor='email'>E-mail</label>
-				<input
-					id='email'
-					onChange={(e) =>
-						setValues({
-							...values,
-							email: e.target.value,
-						})
-					}
-				/>
-				<label htmlFor='password'>Password</label>
-				<input
-					id='password'
-					type='password'
-					onChange={(e) =>
-						setValues({
-							...values,
-							password: e.target.value,
-						})
-					}
-				/>
-				<label htmlFor='passwordRepeat'>Password Repeat</label>
-				<input
-					id='passwordRepeat'
-					type='password'
-					onChange={(e) =>
-						setValues({
-							...values,
-							passwordRepeat: e.target.value,
-						})
-					}
-				/>
-				<button disabled={disabled} onClick={(e) => onSubmit(e)}>
-					Sign Up
-				</button>
+		<div className='col-lg-6 offset-lg-3 col-md-8 offset-md-2'>
+			<form className='card mt-5'>
+				<div className='card-header'>
+					<h1 className='text-center'>Sign Up</h1>
+				</div>
+				<div className='card-body'>
+					<div className='mb-3'>
+						<label htmlFor='username' className='form-label'>
+							Username
+						</label>
+						<input
+							id='username'
+							className='form-control'
+							onChange={(e) =>
+								setValues({
+									...values,
+									username: e.target.value,
+								})
+							}
+						/>
+					</div>
+					<div className='mb-3'>
+						<label htmlFor='email' className='form-label'>
+							E-mail
+						</label>
+						<input
+							id='email'
+							className='form-control'
+							onChange={(e) =>
+								setValues({
+									...values,
+									email: e.target.value,
+								})
+							}
+						/>
+					</div>
+					<div className='mb-3'>
+						<label htmlFor='password' className='form-label'>
+							Password
+						</label>
+						<input
+							id='password'
+							type='password'
+							className='form-control'
+							onChange={(e) =>
+								setValues({
+									...values,
+									password: e.target.value,
+								})
+							}
+						/>
+					</div>
+					<div className='mb-3'>
+						<label htmlFor='passwordRepeat' className='form-label'>
+							Password Repeat
+						</label>
+						<input
+							id='passwordRepeat'
+							className='form-control'
+							type='password'
+							onChange={(e) =>
+								setValues({
+									...values,
+									passwordRepeat: e.target.value,
+								})
+							}
+						/>
+					</div>
+					<div className='text-center'>
+						<button
+							className='btn btn-primary'
+							disabled={disabled}
+							onClick={(e) => onSubmit(e)}>
+							Sign Up
+						</button>
+					</div>
+				</div>
 			</form>
-		</>
+		</div>
 	);
 };
 
